8cab0f9f5622e17e8a20c9249c7321b06a4e7a7f
Adrienne Yao <adrienne.yao@icloud.com>
2021-01-18 15:04:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // fetch the images using the url above ⬆️
 // parse the response as JSON
-// add image elements to the DOM for each🤔 image in the array
 function loadImages() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
   fetch(imgUrl)
@@ -20,7 +19,7 @@ function loadImages() {
     });
 }
 
-
+// add image elements to the DOM for each🤔 image in the array
 function addImage(dogPicUrl) {
   let container = document.querySelector('#dog-image-container');
   let newImageEl = document.createElement('img');
